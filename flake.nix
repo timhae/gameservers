26abb04b1da79962974cf3985edf4a397c0d3ec4
Tim Häring <tim.haering@gmail.com>
2023-01-22 16:06:38 +0100

    feat(stardew-server): add SMAPI, mods are next

--- flake.nix
+++ flake.nix
@@ -3,8 +3,16 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
   };
-  outputs = { self, nixpkgs }: {
-    packages.x86_64-linux.stardew-server = nixpkgs.legacyPackages.x86_64-linux.callPackage ./pkgs/stardew-server.nix { };
-    nixosModules.stardew-server = import ./module/stardew-server.nix;
-  };
+  outputs = { self, nixpkgs }:
+    let
+      callPackage = nixpkgs.legacyPackages.x86_64-linux.callPackage;
+    in
+    {
+      packages.x86_64-linux = {
+        stardew-server = callPackage ./pkgs/stardew-server.nix { };
+      };
+      nixosModules = {
+        stardew-server = import ./module/stardew-server.nix;
+      };
+    };
 }
--- pkgs/stardew-server.nix
+++ pkgs/stardew-server.nix
@@ -1,43 +1,46 @@
-with import <nixpkgs> {};
-{ lib
-, stdenvNoCC
+{ fetchzip
 , steam-run
-, makeWrapper
-, runtimeShell
 , requireFile
+, runtimeShell
+, stdenvNoCC
 , unzip
 }:
 stdenvNoCC.mkDerivation rec {
   pname = "stardew-server";
-
-  version = "0.0.1";
-
+  version = "20220118";
+  smapi-version = "3.18.1";
   src = requireFile rec {
     name = "stardew-server-${version}.zip";
     message = ''
       This nix expression requires that ${name} is already part of the store.
-      Zip the folder in "~/.local/share/Steam/steamapps/common" with
-      "zip -r stardew-server-${version}.zip Stardew\ Valley/*", and add it to
-      the nix store with "nix-prefetch-url file://\$PWD/${name}".
+      Zip the game folder in "~/.local/share/Steam/steamapps/common" with
+      "zip -r ${name} Stardew\ Valley/*", and add it to the nix store with
+      "nix-prefetch-url file://\$PWD/${name}".
+      Make sure that your game version is the same as the one listed here
+      https://steamdb.info/depot/413153/manifests/ with date ${version}.
     '';
-    sha256 = "0sakpfwhh2z7zz6ny32dpdsqdwhx8rrinfs2wphn98jzajsgvvl2";
+    sha256 = "17s6a6xrbgcw34f4rmf5qhlrm7wpqd8jhyik5gdanwr0vs1sxci1";
   };
-
-  buildInputs = [ steam-run ];
-
-  nativeBuildInputs = [ makeWrapper ];
-
+  src-smapi = fetchzip {
+    url = "https://github.com/Pathoschild/SMAPI/releases/download/${smapi-version}/SMAPI-${smapi-version}-installer.zip";
+    sha256 = "sha256-kuPCDKXdD5HL7+7OQgqxSM35At2o901CVGnhaxYLqZ0=";
+  };
   unpackPhase = ''
+    mkdir -p unzip
     ${unzip}/bin/unzip ${src}
+    cp -r Stardew\ Valley/* unzip/
+    rm -rf Stardew\ Valley/
+    ${unzip}/bin/unzip -o ${src-smapi}/internal/linux/install.dat -d unzip
   '';
-
   installPhase = ''
     mkdir -p $out/bin $out/files
-    cp -r Stardew\ Valley/* "$out/files"
+    cp -r unzip/* $out/files
+    cp $out/files/Stardew\ Valley.deps.json $out/files/StardewModdingAPI.deps.json
+    mv $out/files/StardewValley $out/files/StardewValley-original
+    mv $out/files/StardewModdingAPI $out/files/StardewValley
     cat > $out/bin/stardew-server <<EOF
     #!${runtimeShell}
-    export PATH=${steamcmd}/bin:\$PATH
-    exec ${steam-run}/bin/steam-run $out/files/Stardew\ Valley '\$@'
+    exec ${steam-run}/bin/steam-run $out/files/StardewValley
     EOF
     chmod +x $out/bin/stardew-server
   '';
